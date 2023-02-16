1c390a71a5828413a8c7e6893d2c8f0092b0f645
Animila <khristoforov-i@mail.ru>
2023-02-16 17:08:04 +0900

    Работа

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -68,7 +68,34 @@ function App() {
 						<p className='self-center text-xl font-normal'>Онлайн - курсов</p>
 					</div>
 				</div>
-				
+
+				<h3 className='text-white mt-16 text-center text-[30px]'>
+					Варианты сотрудничества
+				</h3>
+				<div className='w-100 mx-4 min-h-[450px] mt-2 rounded-3xl border-white border-2'>
+					<div className='text-white flex flex-col justify-center pb-4 pt-11'>
+						<img
+							src='/assets/various/arms.png'
+							alt=''
+							className='h-[90px] w-[90px] self-center'
+						/>
+						<h4 className='self-center text-[24px]'>Партнер</h4>
+						<p className='self-center text-[10px] font-normal pb-1 text-[#96ADFF] underline'>
+							оставить заявку
+						</p>
+					</div>
+					<div className='text-white flex flex-col justify-center mt-[25px]'>
+						<img
+							src='/assets/various/people.png'
+							alt=''
+							className='h-[90px] w-[90px] self-center pb-1'
+						/>
+						<h4 className='self-center text-[24px]'>Стать разработчиком</h4>
+						<p className='self-center text-[10px] font-normal text-[#96ADFF] underline'>
+							оставить заявку
+						</p>
+					</div>
+				</div>
 			</section>
 
 			{/* <section>
