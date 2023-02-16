6b0d0398b1bb16a1f49735adc61a775c842d1686
Animila <khristoforov-i@mail.ru>
2023-02-16 16:10:42 +0900

    Ссылка

--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -11,17 +11,14 @@ function Chat() {
 	const handleSubmit = async event => {
 		event.preventDefault()
 		console.log(JSON.stringify({ message }))
-		const response = await fetch(
-			'http://api.https://hackaton-yakse.ru/api/chatbot',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
+		const response = await fetch('http://api.hackaton-yakse.ru/api/chatbot', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
 
-				body: JSON.stringify({ message }),
-			}
-		).then(res => res.json())
+			body: JSON.stringify({ message }),
+		}).then(res => res.json())
 		console.log(response.response)
 		setResponse(response.response)
 	}
